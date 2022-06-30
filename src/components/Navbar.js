3bd003581f533d9f847550d29a230052025bed9a
Mostak Ahamed Nishat <37589801+Mostak-Ahamed-Nishat@users.noreply.github.com>
2022-06-30 10:08:59 +0600

    sidebar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,21 +4,15 @@ import {
   Badge,
   Box,
   InputBase,
+  Menu,
+  MenuItem,
   styled,
   Toolbar,
   Typography,
 } from "@mui/material";
-import React from "react";
-// import { alpha } from "@mui/material/styles";
-import {
-  AddCard,
-  AddComment,
-  Email,
-  Notifications,
-  SearchOutlined,
-} from "@mui/icons-material";
-import { bgcolor, color } from "@mui/system";
-// import SearchIcon from "@mui/icons-material/Search";
+import React, { useState } from "react";
+
+import { AddCard, Email, Notifications } from "@mui/icons-material";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
@@ -36,12 +30,40 @@ const SearchBar = styled("div")(({ theme }) => ({
 }));
 
 const Icons = styled(Box)(({ theme }) => ({
-  display: "flex",
-  gap: "15px",
+  display: "none",
+  gap: "20px",
   alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+  "&:hover": {
+    cursor: "pointer",
+  },
 }));
 
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+
+//Change Open Handler
+
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const changeOpen = (e) => {
+    setOpen(true);
+    console.log("Open =" + open);
+  };
+
+  const handleClose = (e) => {
+    setOpen(false);
+  };
+
   return (
     <Box>
       <AppBar position="stick">
@@ -63,7 +85,7 @@ export default function Navbar() {
           />
 
           <SearchBar>
-            <InputBase placeholder="Search.." />
+            <InputBase placeholder="Search.." sx={{ width: "100%" }} />
           </SearchBar>
 
           <Icons>
@@ -75,13 +97,79 @@ export default function Navbar() {
             </Badge>
 
             <Avatar
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={changeOpen}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
               sx={{
                 width: "30ox",
                 height: "40px",
+                "&:hover": {
+                  cursor: "pointer",
+                },
               }}
               src="https://images.pexels.com/photos/11293719/pexels-photo-11293719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             />
           </Icons>
+
+          <UserBox>
+            <Avatar
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={changeOpen}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              sx={{
+                width: "30x",
+                height: "40px",
+                "&:hover": {
+                  cursor: "pointer",
+                },
+              }}
+              src="https://images.pexels.com/photos/11293719/pexels-photo-11293719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+            <Typography variant="span">Nishat</Typography>
+          </UserBox>
+
+          <Menu
+            id="demo-positioned-menu"
+            aria-labelledby="demo-positioned-button"
+            // anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            sx={{
+              marginTop: "38px",
+            }}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
         </StyledToolbar>
       </AppBar>
     </Box>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,22 @@
-import { Box } from "@mui/material";
+import styled from "@emotion/styled";
+import {
+  ArticleSharp,
+  ContactPage,
+  Group,
+  Home,
+  Person,
+  Settings,
+  Storefront,
+} from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
 import React from "react";
 
 export default function Sidebar() {
@@ -11,9 +29,76 @@ export default function Sidebar() {
         },
       }}
       flex={1}
-      bgcolor="lightpink"
     >
-      <h1>This is Sidebar</h1>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+        {/* Item 2 */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <ArticleSharp />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+        {/* Item 3 */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Group />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+        {/* Item 4 */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Storefront />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
+          </ListItemButton>
+        </ListItem>
+        {/* Item 5 */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Person />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+        {/* Item 6 */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        {/* Item 7 */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <ContactPage />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <Switch />
+          <ListItemText primary="Mood" />
+        </ListItem>
+      </List>
     </Box>
   );
 }
